48b43776010f9e15030a676f71271d9d68cd088a
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2023-01-23 08:56:40 -0300

    refactor: add scenarios per user and inter scenarios

--- src/tests/api/users.js
+++ src/tests/api/users.js
@@ -2,12 +2,26 @@ import http from 'k6/http';
 import { group, check } from 'k6';
 
 export const options = {
+    scenarios: {
+        shared_iter_scenario: {
+            executor: "shared-iterations",
+            vus: 10,
+            iterations: 100,
+            startTime: "0s",
+            tags: { users: 'users' },
+        },
+        per_vu_scenario: {
+            executor: "per-vu-iterations",
+            vus: 10,
+            iterations: 10,
+            startTime: "10s",
+        },
+    },
     thresholds: {
         http_req_failed: ['rate<0.01'],
         http_req_duration: ['p(90) < 10'],
-    },
-    vus: 10,
-    duration: '10s',
+
+    }
 };
 
 export default function () {
@@ -17,6 +31,6 @@ export default function () {
         check(res, {
             'is status 200': res.status == 200,
             'is body not empty': res.json().length == 0,
-        });
+        }, { users: 'users' });
     });
 }
